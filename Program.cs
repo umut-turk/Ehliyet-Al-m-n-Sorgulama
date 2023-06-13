15aa2a18513ed00968ef9845ea88989379178173
umut-turk <113532432+umut-turk@users.noreply.github.com>
2023-06-13 21:14:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ehliyet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //yaş  degşeknini  aldık
+            Console.WriteLine("yaş girin");
+            string yas1 = Console.ReadLine();
+            int yas = Convert.ToInt32(yas1);
+
+
+
+
+            //  egtim  durumunu  aldık
+            Console.WriteLine("Egtim  durumunz  nedır?");
+            string ed = Console.ReadLine();
+
+            if (yas >= 18 && ed == "lise")
+            {
+                Console.WriteLine("Ehlıyet  alablrsınız");
+            }
+            else if (yas >= 18 && ed == "üniversite " || ed == "üni")
+            {
+                Console.WriteLine("Ehlıyet  alablrsınız");
+            }
+            else if (yas < 18 || ed == "ilkokul" || ed == "orta okul")
+            {
+                Console.WriteLine("Ehlıyet  alamazsiniz");
+
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
